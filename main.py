02b92edbdf603ae0d8eaf1e6caa744d3a12ec09e
aqsashah <aqsanor665@gmail.com>
2025-07-30 19:40:08 +0500

    Update main.py

--- main.py
+++ main.py
@@ -2,13 +2,13 @@ from mcp.server.fastmcp import FastMCP
 
 mcp = FastMCP(name="hello-mcp", stateless_http=True)
 
-@mcp.tool()
+@mcp.tool(name="online_research", description="Search the web for information")
 def search_online(query: str) -> str:
     # TODO: Implement search logic
     return f"Results for {query}..."
 
-@mcp.tool()
-async def get_weather(city: str) -> str:
-    #TODO: Implement search logic
-    return f"Weather in {city} is sunny."
+# @mcp.tool()
+# async def get_weather(city: str) -> str:
+#     #TODO: Implement search logic
+#     return f"Weather in {city} is sunny."
 mcp_app = mcp.streamable_http_app()
